ae92aefbaa5fd10c9f36c97abec4ca143a43ee90
lovingofbiscuit <46164959+lovingofbiscuit@users.noreply.github.com>
2024-07-08 03:29:25 +0300

    Update ARButton.js

--- ARButton.js
+++ ARButton.js
@@ -51,7 +51,7 @@ class ARButton {
 				button.style.opacity = '1.0';
 			};
 			button.onmouseleave = function () {
-				button.style.opacity = '0.5';
+				button.style.opacity = '0.9';
 			};
 			button.onclick = function () {
 				if ( currentSession === null ) {
@@ -77,14 +77,16 @@ class ARButton {
 		function stylizeElement( element ) {
 			element.style.position = 'absolute';
 			element.style.bottom = '20px';
-			element.style.padding = '12px 6px';
-			element.style.border = '1px solid #fff';
-			element.style.borderRadius = '4px';
-			element.style.background = 'rgba(0,0,0,0.1)';
+			element.style.padding = '12px 24px';
+			element.style.border = 'none';
+			element.style.borderRadius = '8px';
+			element.style.background = '#007bff';
 			element.style.color = '#fff';
-			element.style.font = 'normal 13px sans-serif';
+			element.style.font = 'normal 16px sans-serif';
 			element.style.textAlign = 'center';
-			element.style.opacity = '0.5';
+			element.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
+			element.style.transition = 'background-color 0.3s ease, transform 0.3s ease';
+			element.style.opacity = '0.9';
 			element.style.outline = 'none';
 			element.style.zIndex = '999';
 		}
