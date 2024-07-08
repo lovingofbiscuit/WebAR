bd066d29635ed6a5e19789e3471363cc74dbeb26
lovingofbiscuit <46164959+lovingofbiscuit@users.noreply.github.com>
2024-07-08 03:45:12 +0300

    Update ARButton.js

--- ARButton.js
+++ ARButton.js
@@ -45,7 +45,7 @@ class ARButton {
 			button.style.display = '';
 			button.style.cursor = 'pointer';
 			button.style.left = 'calc(50% - 50px)';
-			button.style.width = '400px';
+			button.style.width = '250px';
 			button.textContent = 'START AR';
 			button.onmouseenter = function () {
 				button.style.opacity = '1.0';
